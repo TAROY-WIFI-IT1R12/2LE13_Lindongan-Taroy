6e559ab8d71630724a9362fe5b0a9598823597b4
venneveni <jovenlindongan@gmail.com>
2026-04-29 16:24:46 +0800

    ers

--- Personal Diary System.py
+++ Personal Diary System.py
@@ -1,1 +1,19 @@
-
+try:
+    f = open("personaldiary.txt","x")
+except FileExistsError:
+    print("File already existed")
+   
+while True:
+   
+    print("Personal Diary System")
+    print("[1] Write a message")
+    print("[2] Add a message")
+    print("[3] Exit")
+   
+    choice = input("Enter your choice: ")
+   
+    if choice == "1":
+        message = input("Enter your message: ")
+        with open("personaldiary.txt","w") as f:
+            f.write(message + "\n")
+        print("Message written to diary.")
