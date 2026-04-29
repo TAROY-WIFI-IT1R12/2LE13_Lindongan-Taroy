025b38d23ece1de15696d4fdf70c1deed8c89b2a
venii <jovenlindongan@gmail.com>
2026-04-30 00:15:20 +0800

    Add read message functionality to diary system

--- Personal Diary System.py
+++ Personal Diary System.py
@@ -8,19 +8,27 @@ while True:
     print("Personal Diary System")
     print("[1] Write a message")
     print("[2] Add a message")
-    print("[3] Exit")
+    print("[3] Read a message")
+    print("[4] Exit")
    
     choice = input("Enter your choice: ")
    
     if choice == "1":
         message = input("Enter your message: ")
-        with open("personaldiary.txt","w") as f:
+        with open("personaldiary.txt", "w") as f:
             f.write(message + "\n")
             print("Message written to diary.")
     elif choice == "2":
         message = input("Enter your message: ")
-        with open("personaldiary.txt","a") as f:
+        with open("personaldiary.txt", "a") as f:
             f.write(message + "\n")
         print("Message added to diary.")
+    elif choice == "3":
+        with open("personaldiary.txt", "r") as f:
+            content = f.read()
+            print("Diary Content:")
+            print(content)
+        
+        
     
     
